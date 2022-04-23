ae3ea447fcf306689a51970bb14b5ad00c30d2c5
qianyi <xuepeng.zhang@qianyi.com>
2022-04-23 22:18:31 +0800

    print aa&&hello world
    vncview ip then you can see the log aaa

--- 0001/bootloader/bootmain.c
+++ 0001/bootloader/bootmain.c
@@ -1,21 +1,22 @@
-#if 1 
+#if 1
 void bootmain(void)
 {
-  short *p = (short *)0xb8000;
+  char *p = (char *)0xb8000;
   int i;
   for (i=0;i<12;i++) {
-    *p++=72|0x700;
+    *p='a';
+    p++;
   }
   
 }
 #else
 void bootmain(void)
 {
-  char *w = "Hellow World!"
+  char *w = "Hellow World!";
   short *p = (short *)0xb8000;
   int i;
   for (i=0;i<12;i++) {
-    *p++=w[i]|0x700;
+    *p++=w[i];
   }
 
 }
