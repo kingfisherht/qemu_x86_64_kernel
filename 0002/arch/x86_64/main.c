0072fe190e20b6e90c002d2f6dbfe389be2a2520
qianyi <xuepeng.zhang@qianyi.com>
2022-04-25 15:09:18 +0800

    fixed the build error

--- 0002/arch/x86_64/main.c
+++ 0002/arch/x86_64/main.c
@@ -3,10 +3,10 @@ char the_cpu[4096] __attribute__((section(".percpu")));
 void bp_main(void)
 {
   char *w="Hello World!";
-  short *p=(short *)0xb8000;
+  char *p=(char *)0xb8000;
   int i;
   for (i=0;i<12;i++) {
-    *p++=w[i]|0x700;
+    *p++=*w++;
   }
   for(;;);
 }
